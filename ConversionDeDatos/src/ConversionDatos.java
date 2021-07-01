8de1332dd7f238a345db4e328c1d53e659f174ed
Berno2143 <atomasbernardino@gmail.com>
2021-07-01 11:27:58 -0500

    Primer commit proyecto java

---
+++ ConversionDeDatos/src/ConversionDatos.java
@@ -0,0 +1,34 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		* Tipos de datos en java
+		* Numericos: byte, short, int, long, float, double
+		* Booleanos (verdadero, falso): Boolean
+		* Caracteres: char
+		* Cadenas: String
+		*/
+		/*
+		* byte 8 bits
+		* short 16 bits
+		* int 32 bits
+		* long 64 bits
+		*/
+		byte numeropequeno = 122;
+		byte otronumeropequeno = 122;
+		int suma = numeropequeno + otronumeropequeno;
+
+		int numero1 = 1000;
+		long numero2 = numero1;
+		float numero3 = numero2;
+
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(suma);
+
+	}
+
+}
